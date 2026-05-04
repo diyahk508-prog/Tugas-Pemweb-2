c07e82854e275933bb1950a6b1c2924abd9fee2b
diyahk508-prog <diyahk508@gmail.com>
2026-05-04 17:43:56 +0700

    Update delete.php

--- modules/anggota/delete.php
+++ modules/anggota/delete.php
@@ -6,9 +6,10 @@ $id = $_GET['id'];
 $data = $conn->query("SELECT foto FROM anggota WHERE id_anggota=$id")->fetch_assoc();
 
 if ($data['foto'] && file_exists("uploads/".$data['foto'])) {
-unlink("uploads/".$data['foto']);
+    unlink("uploads/".$data['foto']);
 }
 
 $conn->query("DELETE FROM anggota WHERE id_anggota=$id");
 
 header("Location: index.php");
+exit;
